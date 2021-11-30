419977bae17c1d8b116883d425389ca77952af74
tubaman48 <rjlovett48@gmail.com>
2021-11-30 08:36:59 +0000

    tidy up checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -11,17 +11,28 @@ from products.models import Product
 
 class Order(models.Model):
     """ Order class """
-    order_number = models.CharField(max_length=32, null=False, editable=False)
-    full_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
-    postcode = models.CharField(max_length=20, blank=True)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, blank=True)
-    county = models.CharField(max_length=80, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
+    order_number = models.CharField(
+        max_length=32, null=False, editable=False)
+    full_name = models.CharField(
+        max_length=50, null=False, blank=False)
+    email = models.EmailField(
+        max_length=254, null=False, blank=False)
+    phone_number = models.CharField(
+        max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(
+        max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(
+        max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(
+        max_length=40, null=False, blank=False)
+    county = models.CharField(
+        max_length=80, null=True, blank=True)
+    postcode = models.CharField(
+        max_length=20, null=True, blank=True)
+    country = models.CharField(
+        max_length=40, null=False, blank=False)
+    date = models.DateTimeField(
+        auto_now_add=True)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(
@@ -72,7 +83,7 @@ class OrderLineItem(models.Model):
         Product, null=False, blank=False,
         on_delete=models.CASCADE)
     product_size = models.CharField(  # XS, S, M, L, XL
-        max_length=2, blank=True)
+        max_length=2, null=True, blank=True)
     quantity = models.IntegerField(
         null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
