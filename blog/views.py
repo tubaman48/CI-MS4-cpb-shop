a80c5825760f242494567a8398144587d79f60dc
tubaman48 <rjlovett48@gmail.com>
2022-01-08 23:40:19 +0000

    add add_post blog functionality

--- blog/views.py
+++ blog/views.py
@@ -48,8 +48,8 @@ def post_detail(request, slug):
 def add_post(request):
     """ Add a post to the blog """
     if not request.user.is_superuser:
-        messages.error(request, 'Only our BUBBLES team has access to this.')
-        return redirect(reverse('homepage'))
+        messages.error(request, 'Only our CPB Shop Admin has access to this.')
+        return redirect(reverse('home'))
 
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
@@ -75,19 +75,19 @@ def add_post(request):
 def edit_post(request, slug):
     """ Edit an existing Post """
     if not request.user.is_superuser:
-        messages.error(request, 'Only our BUBBLES team has access to this.')
-        return redirect(reverse('homepage'))
+        messages.error(request, 'Only our CPB Shop Admin has access to this.')
+        return redirect(reverse('home'))
 
     post = get_object_or_404(Post, slug=slug)
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully updated  post!')
+            messages.success(request, 'Successfully updated post!')
             return redirect(reverse('post_detail', args=[post.slug]))
         else:
             messages.error(
-                request, 'Failed to update blog post.check if form is valid.')
+                request, 'Failed to update blog post. Check if form is valid.')
     else:
         form = PostForm(instance=post)
         messages.info(request, f'You are editing {post.title}')
@@ -105,8 +105,8 @@ def edit_post(request, slug):
 def delete_post(request, slug):
     """ Delete a post from the blog """
     if not request.user.is_superuser:
-        messages.error(request, 'Only our BUBBLES team has access to this.')
-        return redirect(reverse('homepage'))
+        messages.error(request, 'Only our CPB Shop Admin has access to this.')
+        return redirect(reverse('home'))
 
     post = get_object_or_404(Post, slug=slug)
     post.delete()
